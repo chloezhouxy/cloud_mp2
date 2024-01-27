740f1554a1faa981f71df4a90929bc4f707f24fa
chloezhouxy <chloezhouxy@gmail.com>
2024-01-26 21:42:05 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -11,6 +11,7 @@ def get_ip_address():
 @app.route('/', methods=['POST'])
 def trigger():
    subprocess.Popen('Python3 stress_cpu.py', shell=True)
+   return 'success'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
